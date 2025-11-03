080e3cfcb453f4c8a6a3e41165c69cf31364fc4b
Alexandre Le Bars <alexlb329@gmail.com>
2025-11-03 22:26:50 +0100

    réparation

--- scripts/addTreeTypes.lua
+++ scripts/addTreeTypes.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- LP and BMP
 
 addTreeTypes = {}
@@ -22,29 +21,4 @@ function addTreeTypes.loadDefaultTypes(self, superFunc, missionInfo, baseDirecto
     delete(xmlFile)
 end
 
-=======
--- LP and BMP
-
-addTreeTypes = {}
-
-local modDir = g_currentModDirectory
-local modName = g_currentModName
-
-function addTreeTypes.loadDefaultTypes(self, superFunc, missionInfo, baseDirectory)
-    -- Call the original function to load default tree types
-    superFunc(self, missionInfo, baseDirectory)
-
-    -- Set maxNumTrees to 45000
-    g_treePlantManager.maxNumTrees = 45000
-    Logging.info("[addTreeTypes] Set maxNumTrees to 45000")
-
-    -- Load the custom tree types XML file
-    local xmlFile = loadXMLFile("treeTypes", modDir .. "maps_treeTypes.xml")
-    g_treePlantManager:loadTreeTypes(xmlFile, missionInfo, modDir, false)
-
-    -- Clean up
-    delete(xmlFile)
-end
-
->>>>>>> 44bf5fb (latest version)
 TreePlantManager.loadDefaultTypes = Utils.overwrittenFunction(TreePlantManager.loadDefaultTypes, addTreeTypes.loadDefaultTypes)
